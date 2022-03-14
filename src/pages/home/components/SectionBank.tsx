63574f54aed171e0ae2ad7de99f1c1ae7019b36d
Long Le Ngoc <longln@tohsoft.com>
2022-03-14 14:08:56 +0700

    update show list stake and claim action

--- src/pages/home/components/SectionBank.tsx
+++ src/pages/home/components/SectionBank.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { Fragment, useEffect } from 'react'
 import { useState } from 'react'
 import { FC } from 'react'
 import { Element } from 'react-scroll'
@@ -9,8 +9,9 @@ import { Button, Icon, InputNumber, Message } from '../../../components'
 import { InputTagSelect } from '../../../components/input/tag-select'
 import { DateTimeUtils, InputWraper, NumberUtils, useForm } from '../../../modules'
 import { AppService, ESMCStatus, SmcService } from '../../../services'
-import { StakePackage, StakingService, UserStake } from '../../../services/staking'
+import { StakePackage, UserStake } from '../../../services/staking'
 import { StakingServiceV2 } from '../../../services/staking/stakingv2.service'
+import _ from 'lodash';
 
 export const SectionBank: FC = () => {
 	return (
@@ -19,9 +20,7 @@ export const SectionBank: FC = () => {
 				<div className="sectionTitle">Staking</div>
 
 				<div className="row justify-content-center">
-					<div className="col-sm-5">
-						<Form />
-					</div>
+					<Form />
 				</div>
 			</div>
 		</Element>
@@ -132,7 +131,7 @@ const Form: FC = () => {
 		})
 			.then(res => {
 				console.log('getMyStakingData: ', res)
-				// if (!(+res.initial)) return setUserStake(null);
+				setUserStake(res)
 				// setUserStake({
 				// 	initial: +NumberUtils.cryptoConvert('decode', +res.initial, SmcService.contractUBGToken._decimals),
 				// 	reward: +NumberUtils.cryptoConvert('decode', +res.reward, SmcService.contractUBGToken._decimals),
@@ -215,80 +214,164 @@ const Form: FC = () => {
 		setIsClaiming(false);
 	}
 
-	return <form onSubmit={handleSubmit}>
-		<div className="head">
-			<div className="title">Investment</div>
-			<div className="UserBalance">
-				<span className="icon"><Icon.Wallet /></span>
-				<span className="label">Your balance:</span>
-				<span className="value">{balance === null ? '--' : `${balance.toLocaleString(getLocaleKey(true), { maximumFractionDigits: 9 })} UBG`}</span>
-			</div>
-		</div>
-
-		{function () {
-			if (smc.error) return <div className="block">
-				<Message type="INFO" content={smc.error} />
-			</div>
-
-			if (!isFetched) return <div className="Loading">
-				<Icon.Loading />
-				Loading...
-			</div>
-
-			const packageActive = packages.find(v => v.id === values.packageId) as StakingServiceV2;
-			console.log('packageActive: ', packageActive)
-			//const reward = values.amount ? (values.amount * packageActive.interest) / 100 : 0;
-			const reward = values.amount ? (packageActive.interest / 1000 + 1) * values.amount : 0;
-
-			if (userStake) return <>
-				<div className="UserStake">
-					<div className="title">UBG's saving information</div>
-
-					<div className="content">
-						<div className="rowInfo">
-							<div className="label">Saving</div>
-							<div className="value">{userStake.initial.toLocaleString(getLocaleKey(true), { maximumFractionDigits: 2 })} UBG</div>
-						</div>
-						<div className="rowInfo">
-							<div className="label">Reward</div>
-							<div className="value">{userStake.reward.toLocaleString(getLocaleKey(true), { maximumFractionDigits: 2 })} UBG</div>
-						</div>
-						<div className="rowInfo">
-							<div className="label">Start Day</div>
-							<div className="value">{DateTimeUtils.formatToShow(userStake.startAt, true, getLocaleKey(true))}</div>
-						</div>
-						<div className="rowInfo">
-							<div className="label">Pay Day</div>
-							<div className="value">
-								<p>{DateTimeUtils.formatToShow(userStake.payAt, true, getLocaleKey(true))}</p>
-								<p className="time">{countDownShow(userStake.payAt, new Date(now))}</p>
-							</div>
+	return (
+		<Fragment>
+			<div className="col-sm-5">
+				<form onSubmit={handleSubmit}>
+					<div className="head">
+						<div className="title">Investment</div>
+						<div className="UserBalance">
+							<span className="icon"><Icon.Wallet /></span>
+							<span className="label">Your balance:</span>
+							<span className="value">{balance === null ? '--' : `${balance.toLocaleString(getLocaleKey(true), { maximumFractionDigits: 9 })} UBG`}</span>
 						</div>
 					</div>
-				</div>
-			</>
 
-			return <>
-				<InputWraper inputProps={inputProps.amount} component={InputNumber} />
-				<InputWraper inputProps={inputProps.packageId} component={InputTagSelect} className="hideBorder" />
+					{function () {
+						if (smc.error) return <div className="block">
+							<Message type="INFO" content={smc.error} />
+						</div>
 
-				<p className="note">After <strong className="textPrimary">{packageActive.numberOfDays} days</strong> with <strong className="textPrimary">{values.amount ? (+values.amount).toLocaleString(getLocaleKey(true), { maximumFractionDigits: 2 }) : '--'} UBG</strong>, you will able to claim <strong className="textPrimary">{reward ? reward.toLocaleString(getLocaleKey(true), { maximumFractionDigits: 2 }) : '--'} UBG</strong></p>
-			</>
-		}()}
+						if (!isFetched) return <div className="Loading">
+							<Icon.Loading />
+							Loading...
+						</div>
 
-		<div className="cta">
-			{function () {
-				if (smc.error) return
-				if (smc.status === ESMCStatus.NONE) return;
-				if (smc.status !== ESMCStatus.READY) return <Button label="Connect Wallet" buttonType="warning" onClick={() => SmcService.handleConnectWallet()} />
-				if (userStake) {
-					const isClaimActive = new Date(now).getTime() >= new Date(userStake.payAt).getTime();
-					return <Button label="Claim" isLoading={isClaiming} onClick={handleClaim(1)} disabled={!isClaimActive} />;
+						const packageActive = packages.find(v => v.id === values.packageId) as StakingServiceV2;
+						//const reward = values.amount ? (values.amount * packageActive.interest) / 100 : 0;
+						const reward = values.amount ? (packageActive.interest / 10000 + 1) * values.amount : 0;
+
+						// if (userStake) return <>
+							
+						// </>
+
+						return <>
+							<Fragment>
+								<InputWraper inputProps={inputProps.amount} component={InputNumber} />
+								<InputWraper inputProps={inputProps.packageId} component={InputTagSelect} className="hideBorder" />
+
+								<p className="note">After <strong className="textPrimary">{packageActive.numberOfDays} days</strong> with <strong className="textPrimary">{values.amount ? (+values.amount).toLocaleString(getLocaleKey(true), { maximumFractionDigits: 2 }) : '--'} UBG</strong>, you will able to claim <strong className="textPrimary">{reward ? reward.toLocaleString(getLocaleKey(true), { maximumFractionDigits: 2 }) : '--'} UBG</strong></p>
+								
+							</Fragment>
+						</>
+					}()}
+
+					<div className="cta">
+						{function () {
+							if (smc.error) return
+							if (smc.status === ESMCStatus.NONE) return;
+							if (smc.status !== ESMCStatus.READY) return <Button label="Connect Wallet" buttonType="warning" onClick={() => SmcService.handleConnectWallet()} />
+							// if (userStake) {
+							// 	const isClaimActive = new Date(now).getTime() >= new Date(userStake.payAt).getTime();
+							// 	return <Button label="Claim" isLoading={isClaiming} onClick={handleClaim(1)} disabled={!isClaimActive} />;
+							// }
+							return <Button isLoading={isSubmitting} type="submit" label="Stake" />
+						}()}
+					</div>
+				</form>
+			</div>
+			
+			<div className="user-stake-wrapper">
+				<div className="main-title text-center">Your UBG's saving information</div>
+				{!_.isEmpty(userStake) &&
+					<div className="row">
+						{_.map(userStake, (s, i) => {
+							const currentTime = new Date().getTime()
+							
+ 							let reward = (s.interest / 10000 + 1) * s.amount;
+							let timestamp = s.startTime*1000
+							let date = new Date(parseInt(timestamp));
+							let endDate = new Date(parseInt(timestamp + s.duration*1000));
+
+							let showDate = date.getDate()+
+							"/"+(date.getMonth()+1)+
+							"/"+date.getFullYear()+
+							" "+date.getHours()+
+							":"+date.getMinutes()+
+							":"+date.getSeconds();
+
+							let showEndDate = endDate.getDate()+
+							"/"+(endDate.getMonth()+1)+
+							"/"+endDate.getFullYear()+
+							" "+endDate.getHours()+
+							":"+endDate.getMinutes()+
+							":"+endDate.getSeconds();
+
+
+							// toLocaleString(getLocaleKey(true), { maximumFractionDigits: 9 })
+
+							return (
+								<Fragment>
+									{s.amount > 0 && 
+										<div className="col-12 col-md-3" index={i}>
+											<div className="user-stake-item">
+												<img src="./images/pool.png" alt="" className="img-fluid" />
+												<div className="stake-infomation">
+													<div className="rowInfo">
+														<div className="label">Saving</div>
+														<div className="value">{s.amount / 1e9} UBG</div>
+													</div>
+													<div className="rowInfo">
+														<div className="label">Reward</div>
+														<div className="value">{reward / 1e9 - s.amount / 1e9} UBG</div>
+													</div>
+													<div className="rowInfo">
+														<div className="label">Start Day</div>
+														<div className="value">{showDate}</div>
+													</div>
+													<div className="rowInfo">
+														<div className="label">Start Day</div>
+														<div className="value">{showEndDate}</div>
+													</div>
+												</div>
+												<div className="stake-action">
+												{function () {
+													if (smc.error) return
+													if (smc.status === ESMCStatus.NONE) return;
+													if (smc.status !== ESMCStatus.READY) return <Button label="Connect Wallet" buttonType="warning" onClick={() => SmcService.handleConnectWallet()} />
+													const isClaimActive = currentTime >= endDate.getTime();
+													return <Button label="Claim" isLoading={isClaiming} onClick={() => handleClaim(s.id)} disabled={!isClaimActive} />;
+												}()}
+												</div>
+											</div>
+										</div>
+									}
+								</Fragment>
+								
+							)
+						})}
+						
+					</div>
+					// <div className="UserStake">
+					// 	<div className="title">UBG's saving information</div>
+
+					// 	<div className="content">
+					// 		<div className="rowInfo">
+					// 			<div className="label">Saving</div>
+					// 			<div className="value">{userStake.initial.toLocaleString(getLocaleKey(true), { maximumFractionDigits: 2 })} UBG</div>
+					// 		</div>
+					// 		<div className="rowInfo">
+					// 			<div className="label">Reward</div>
+					// 			<div className="value">{userStake.reward.toLocaleString(getLocaleKey(true), { maximumFractionDigits: 2 })} UBG</div>
+					// 		</div>
+					// 		<div className="rowInfo">
+					// 			<div className="label">Start Day</div>
+					// 			<div className="value">{DateTimeUtils.formatToShow(userStake.startAt, true, getLocaleKey(true))}</div>
+					// 		</div>
+					// 		<div className="rowInfo">
+					// 			<div className="label">Pay Day</div>
+					// 			<div className="value">
+					// 				<p>{DateTimeUtils.formatToShow(userStake.payAt, true, getLocaleKey(true))}</p>
+					// 				<p className="time">{countDownShow(userStake.payAt, new Date(now))}</p>
+					// 			</div>
+					// 		</div>
+					// 	</div>
+					// </div>
 				}
-				return <Button isLoading={isSubmitting} type="submit" label="Stake" />
-			}()}
-		</div>
-	</form>
+			</div>
+			
+		</Fragment>
+	)
 }
 
 const countDownShow = (startTime: Date, endTime: Date) => {
