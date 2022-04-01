6eece263201d0c2bc85715ccd35bb9fc72fd15fa
Long Le Ngoc <longln@tohsoft.com>
2022-04-02 00:02:00 +0700

    fix gas price

--- src/services/smc/smc.service.tsx
+++ src/services/smc/smc.service.tsx
@@ -328,8 +328,10 @@ export class SmcService {
                     })
                 );
 
+            const estimateGas = options.params.value ? {from: SmcService.address, value: options.params.value} : {from: SmcService.address}
+
             const gas = await func(...args)
-                .estimateGas({from: SmcService.address})
+                .estimateGas(estimateGas)
                 .then((res: number) => +res * 2)
                 .catch((err: any) => {
                     reject(err);
